d9df77d37380808ce0c0f13e28983bd89d47b4cd
chuanshaof <chuanshaof@gmail.com>
2023-10-01 02:00:57 +0800

    added cors for flask app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from allocation import allocation
 from model import model
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/ingestor", methods=["POST"])
 def ingestor():
